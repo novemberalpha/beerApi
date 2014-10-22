bab27c2725caa3453c05c4bf590583d864b68e55
Joseph Juhnke <jjuhnke@shoppertrak.com>
2014-10-22 15:58:35 -0500

    add Oauth2 client support

--- server.js
+++ server.js
@@ -5,13 +5,25 @@ var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
 var passport = require('passport');
 var authController = require('./controllers/auth');
+var clientController = require('./controllers/client');
+var ejs = require('ejs');
+var session = require('express-session');
+var oauth2Controller = require('./controllers/oauth2');
 
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({
   extended: true
+}));
+
+app.use(session({
+  secret: 'Super Secret Session Key',
+  saveUninitialized: true,
+  resave: true
 }));
 
 app.use(passport.initialize());
@@ -19,10 +31,6 @@ app.use(passport.initialize());
 var router = express.Router();
 
 //START ROUTES
-
-router.route('/users')
-  .post(userController.postUsers)
-  .get(authController.isAuthenticated, userController.getUsers);
 
 router.route('/beers')
   .post(authController.isAuthenticated, beerController.postBeers)
@@ -33,8 +41,23 @@ router.route('/beers/:beer_id')
   .put(authController.isAuthenticated, beerController.putBeer)
   .delete(authController.isAuthenticated, beerController.deleteBeer);
 
+router.route('/users')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
+
+router.route('/clients')
+  .post(authController.isAuthenticated, clientController.postClients)
+  .get(authController.isAuthenticated, clientController.getClients);
+
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
+
 //END ROUTES
 
-app.use('/api', router);
+app.use('/api/v1', router);
 
 app.listen(3000);
